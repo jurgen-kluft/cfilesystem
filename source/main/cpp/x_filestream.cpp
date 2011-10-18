603b6f2bbb6114f66a8714cd4ac45fb53a71af84
logan <logan@cnshaw666.virtuosgames.com>
2011-10-18 18:24:05 +0800

    Change some code to look into this issue: using a file operation to get the current file pointer in the main thread, while writing in the IO thread.  This operation is not thread safe

--- source/main/cpp/x_filestream.cpp
+++ source/main/cpp/x_filestream.cpp
@@ -393,14 +393,15 @@ namespace xcore
 		{
 			if (mCaps.isSet(USE_WRITE))
 			{
-				u64 p = getPosition();
+				// TODO: fix proper offset??
+				u64 p; //= getPosition(); // This is not synchronized, must do this in the io thread NOT main thread!!!!
 
 				x_asyncio_callback_struct currCallback = mCallback;
 
 				if(callback.callback != NULL)
 					currCallback = callback;
 
-				xfilesystem::xfs_common::s_instance()->write(mFileHandle, p, count, &buffer[offset], currCallback);
+				xfilesystem::xfs_common::s_instance()->write(mFileHandle, 0, count, &buffer[offset], currCallback);
 				return true;	
 			}
 			return false;
