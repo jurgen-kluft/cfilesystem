2db7dd37efd7501c0b7722bb4320519b919608d3
logan <logan@cnshaw666.virtuosgames.com>
2011-10-28 18:12:00 +0800

    Finish the implementation of wii nand device.  Most important functions should work (not tested yet)

--- source/main/cpp/x_filedevice_wii.cpp
+++ source/main/cpp/x_filedevice_wii.cpp
@@ -51,6 +51,7 @@ namespace xcore
 
 			DVDFileInfo*		mDvdFileInfo;
 			u32					mNumOpenFiles;
+			u32					mMaxNumOpenFiles;
 
 			mutable xcore::atomic::queue<u32> mFreeFileinfoIndecies;
 			//xcore::atomic::queue<u32> mUsedFileinfoIdecies;
@@ -124,6 +125,7 @@ namespace xcore
 			//mDvdFileInfo = new DVDFileInfo[xfs_common::s_instance()->getMaxOpenFiles()];
 			mDvdFileInfo = (DVDFileInfo*)heapAlloc(sizeof(DVDFileInfo) * xfs_common::s_instance()->getMaxOpenFiles(), 4);
 			mNumOpenFiles = 0;
+			mMaxNumOpenFiles = xfs_common::s_instance()->getMaxOpenFiles();
 
 			mFreeFileinfoIndecies.init(getAllocator(), xfs_common::s_instance()->getMaxOpenFiles());
 			//mUsedFileinfoIdecies.init(getAllocator(), xfs_common::s_instance()->getMaxOpenFiles());
@@ -194,8 +196,14 @@ namespace xcore
 
 		bool FileDevice_WII_DVD::getLengthOfFile(u32 nFileHandle, u64& outLength) const
 		{
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				return false;
+			}
+
 			u64 l = (u64)DVDGetLength(&mDvdFileInfo[nFileHandle]);
-			l = (l + 31) & 0xffffffe0;											///< DVDRead requires size to be a multiple of 32 so we better return a file size that fulfills this requirement
+			// if the file exists, dvdgetlength should already give us a 32-divisible number...
+			//l = (l + 31) & 0xffffffe0;											///< DVDRead requires size to be a multiple of 32 so we better return a file size that fulfills this requirement
 			outLength = l;
 			return true;
 		}
@@ -207,6 +215,11 @@ namespace xcore
 
 		bool FileDevice_WII_DVD::closeFile(u32 nFileHandle) const
 		{
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				return false;
+			}
+
 			DVDFileInfo* dvdFileInfo = &mDvdFileInfo[nFileHandle];
 			xbool boSuccess = DVDClose(dvdFileInfo);
 			
@@ -232,6 +245,13 @@ namespace xcore
 
 		bool FileDevice_WII_DVD::readFile(u32 nFileHandle, u64 pos, void* buffer,  u64 count, u64& outNumBytesRead) const
 		{
+
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				outNumBytesRead = 0;
+				return false;
+			}
+
 			DVDFileInfo* dvdFileInfo = &mDvdFileInfo[nFileHandle];
 			count = (count + 31) & 0xffffffe0;									///< DVDRead requires count to be a multiple of 32
 			s32 numBytesRead = DVDRead(dvdFileInfo, buffer, count, (s32)pos);
@@ -245,7 +265,7 @@ namespace xcore
 
 		bool FileDevice_WII_DVD::writeFile(u32 nFileHandle, u64 pos, const void* buffer,  u64 count, u64& outNumBytesWritten) const
 		{
-			DVDFileInfo* dvdFileInfo = &mDvdFileInfo[nFileHandle];
+			//DVDFileInfo* dvdFileInfo = &mDvdFileInfo[nFileHandle];
 			// Unable to write
 			
 			//ASSERT(0);
@@ -254,10 +274,32 @@ namespace xcore
 
 		bool FileDevice_WII_DVD::seek(u32 nFileHandle, ESeekMode mode, u64 pos, u64& newPos) const
 		{
+
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				return false;
+			}
+
 			DVDFileInfo* dvdFileInfo = &mDvdFileInfo[nFileHandle];
 			if (mode == __SEEK_END)
+			{
 				pos = (u64)DVDGetLength(dvdFileInfo) - pos;
-			pos = (pos + 3) & 0xfffffffc;
+				pos = (pos + 3) & 0xfffffffc; // handle multiple of 4?
+			}
+			else if(mode == __SEEK_CURRENT)
+			{
+				if(pos %4 != 0 && pos != 0)
+				{
+					pos = (pos + 3) & 0xfffffffc; // handle multiple of 4?
+				}
+				pos = dvdFileInfo->cb.offset + pos;
+			}
+			else
+			{
+				pos = (pos + 3) & 0xfffffffc; // handle multiple of 4?
+			}
+
+			
 			s32 nResult = DVDSeek(dvdFileInfo, (s32)pos);
 			if (nResult==0)
 				newPos = pos;
@@ -581,6 +623,7 @@ namespace xcore
 		{
 			NANDFileInfo*		mNandFileInfo;
 			u32					mNumOpenFiles;
+			u32					mMaxNumOpenFiles;
 
 			mutable xcore::atomic::queue<u32> mFreeFileinfoIndecies;
 			//xcore::atomic::queue<u32> mUsedFileinfoIdecies;
@@ -589,10 +632,12 @@ namespace xcore
 			FileDevice_WII_NAND()
 			{
 				mFreeFileinfoIndecies.init(getAllocator(), xfs_common::s_instance()->getMaxOpenFiles());
-				mNandFileInfo = new NANDFileInfo[xfs_common::s_instance()->getMaxOpenFiles()];
+				//mNandFileInfo = new NANDFileInfo[xfs_common::s_instance()->getMaxOpenFiles()];
+				mNandFileInfo = (NANDFileInfo*)heapAlloc(xfs_common::s_instance()->getMaxOpenFiles() * sizeof(NANDFileInfo), 4);
 				mNumOpenFiles = 0;
+				mMaxNumOpenFiles = xfs_common::s_instance()->getMaxOpenFiles();
 
-				for(s32 i = 0; i < xfs_common::s_instance()->getMaxOpenFiles(); i++)
+				for(s32 i = 0; i < mMaxNumOpenFiles; i++)
 				{
 					u32 freeFileIndex = (u32)i;
 					mFreeFileinfoIndecies.push(freeFileIndex);
@@ -601,7 +646,8 @@ namespace xcore
 			}
 			virtual					~FileDevice_WII_NAND()								
 			{
-				delete [] mNandFileInfo;
+				//delete [] mNandFileInfo;
+				heapFree(mNandFileInfo);
 			}
 
 			virtual bool			canSeek() const										{ return true; }
@@ -642,10 +688,10 @@ namespace xcore
 
 
 			enum ESeekMode { __SEEK_ORIGIN = 1, __SEEK_CURRENT = 2, __SEEK_END = 3, };
-			bool					seek(u32 nFileHandle, ESeekMode mode, u64 pos, u64& newPos);
-			bool					seekOrigin(u32 nFileHandle, u64 pos, u64& newPos);
-			bool					seekCurrent(u32 nFileHandle, u64 pos, u64& newPos);
-			bool					seekEnd(u32 nFileHandle, u64 pos, u64& newPos);
+			bool					seek(u32 nFileHandle, ESeekMode mode, u64 pos, u64& newPos) const;
+			bool					seekOrigin(u32 nFileHandle, u64 pos, u64& newPos) const;
+			bool					seekCurrent(u32 nFileHandle, u64 pos, u64& newPos) const;
+			bool					seekEnd(u32 nFileHandle, u64 pos, u64& newPos) const;
 
 			bool					init();
 			bool					exit();
@@ -738,8 +784,38 @@ namespace xcore
 			return (result == NAND_RESULT_OK);
 		}
 
+
+		bool FileDevice_WII_NAND::createFile( const char* szFilename, bool boRead, bool boWrite, u32& nFileHandle) const
+		{
+
+			
+			return openFile(szFilename, true, true, nFileHandle);
+		}
+
+		bool FileDevice_WII_NAND::getDeviceInfo(u64& totalSpace, u64& freeSpace) const
+		{
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::hasFile(const char* szFilename) const
+		{
+			u32 fileHandle;
+			if(openFile(szFilename, true, false, fileHandle))
+			{
+				closeFile(fileHandle);
+				return true;
+			}
+
+			return false;
+		}
+
 		bool FileDevice_WII_NAND::getLengthOfFile(u32 nFileHandle, u64& outLength) const
 		{
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				return false;
+			}
+
 			NANDFileInfo* fileInfo = &mNandFileInfo[nFileHandle];
 
 			unsigned long length = 0;
@@ -753,6 +829,12 @@ namespace xcore
 
 		bool FileDevice_WII_NAND::closeFile(u32 nFileHandle) const
 		{
+
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				return false;
+			}
+
 			NANDFileInfo* fileInfo = &mNandFileInfo[nFileHandle];
 
 			s32 result = NANDClose(fileInfo);
@@ -775,12 +857,19 @@ namespace xcore
 
 		bool FileDevice_WII_NAND::readFile(u32 nFileHandle, u64 pos, void* buffer, u64 count, u64& outNumBytesRead) const
 		{
+
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				outNumBytesRead = 0;
+				return false;
+			}
+
 			NANDFileInfo* fileInfo = &mNandFileInfo[nFileHandle];
 
 			outNumBytesRead = 0;
 
 			// make sure size is multiple of 32
-			if (count % 32 == 0)
+			count = (count + 31) & 0xffffffe0;									///< NANDread requires count to be a multiple of 32
 			{
 				s32 result = NANDRead(fileInfo, buffer, (u32)count);
 				__CheckNANDError(result);
@@ -789,15 +878,16 @@ namespace xcore
 					outNumBytesRead = (u64)result;
 			}
 
-			else
-			{
-				x_printf("ERROR: Reading from file %d on Wii NAND but size was %d not multiple of 32 (remainder=%d)\n", nFileHandle, count, count % 32);
-			}
-
 			return (outNumBytesRead != 0);
 		}
 		bool FileDevice_WII_NAND::writeFile(u32 nFileHandle, u64 pos, const void* buffer, u64 count, u64& outNumBytesWritten) const
 		{
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				outNumBytesWritten = 0;
+				return false;
+			}
+
 			NANDFileInfo* fileInfo = &mNandFileInfo[nFileHandle];
 
 			outNumBytesWritten =  0;
@@ -814,13 +904,20 @@ namespace xcore
 			else
 			{
 				x_printf("ERROR: Writing to file %d on Wii NAND but size was %d not multiple of 32 (remainder=%d)\n", nFileHandle, count, count % 32);
+				ASSERT(0);
 			}
 
 			return (outNumBytesWritten != 0);
 		}
 
-		bool FileDevice_WII_NAND::seek(u32 nFileHandle, ESeekMode mode, u64 pos, u64& newPos)
+		bool FileDevice_WII_NAND::seek(u32 nFileHandle, ESeekMode mode, u64 pos, u64& newPos) const
 		{
+
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				return false;
+			}
+
 			NANDFileInfo* fileInfo = &mNandFileInfo[nFileHandle];
 
 			s32 whence = NAND_SEEK_CUR;
@@ -835,27 +932,329 @@ namespace xcore
 			s32 result = NANDSeek(fileInfo, (s32)pos, whence);
 			__CheckNANDError(result);
 
-			if (result >= 0)
-				newPos = result;
-			else
-				newPos = pos;
+			::u32 fileCurrPos = 0;
+			result = NANDTell(fileInfo, &fileCurrPos);
+
+			newPos = fileCurrPos;
 
 			return (result >= 0);
 		}
-		bool	FileDevice_WII_NAND::seekOrigin(u32 nFileHandle, u64 pos, u64& newPos)
+		bool	FileDevice_WII_NAND::seekOrigin(u32 nFileHandle, u64 pos, u64& newPos) const
 		{
 			return seek(nFileHandle, __SEEK_ORIGIN, pos, newPos);
 		}
 
-		bool	FileDevice_WII_NAND::seekCurrent(u32 nFileHandle, u64 pos, u64& newPos)
+		bool	FileDevice_WII_NAND::seekCurrent(u32 nFileHandle, u64 pos, u64& newPos) const
 		{
 			return seek(nFileHandle, __SEEK_CURRENT, pos, newPos);
 		}
 
-		bool	FileDevice_WII_NAND::seekEnd(u32 nFileHandle, u64 pos, u64& newPos)
+		bool	FileDevice_WII_NAND::seekEnd(u32 nFileHandle, u64 pos, u64& newPos) const
 		{
 			return seek(nFileHandle, __SEEK_END, pos, newPos);
 		}
+
+
+		bool FileDevice_WII_NAND::moveFile(const char* szFilename, const char* szToFilename) const
+		{
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::copyFile(const char* szFilename, const char* szToFilename, bool boOverwrite) const
+		{
+				return false;
+
+
+		}
+
+		bool FileDevice_WII_NAND::setFilePos(u32 nFileHandle, u64& ioPos) const
+		{
+			u64 newPos;
+			return seek(nFileHandle, __SEEK_CURRENT, ioPos, newPos);
+		}
+
+		bool FileDevice_WII_NAND::getFilePos(u32 nFileHandle, u64& outPos) const
+		{
+			if(nFileHandle >= mMaxNumOpenFiles)
+			{
+				return false;
+			}
+
+			NANDFileInfo* fileInfo = &mNandFileInfo[nFileHandle];
+
+			::u32 seekOutPos = 0;
+			::s32 result = NANDTell(fileInfo, &seekOutPos);
+
+			outPos = seekOutPos;
+
+			__CheckNANDError(result);
+
+			return result == NAND_RESULT_OK;
+		}
+
+		bool FileDevice_WII_NAND::setFileTime(const char* szFilename, const xdatetime& creationTime, const xdatetime& lastAccessTime, const xdatetime& lastWriteTime) const
+		{
+
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::getFileTime(const char* szFilename, xdatetime& outCreationTime, xdatetime& outLastAccessTime, xdatetime& outLastWriteTime) const
+		{
+
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::setFileAttr(const char* szFilename, const xattributes& attr) const
+		{
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::getFileAttr(const char* szFilename, xattributes& attr) const
+		{
+			attr.setArchive(true);
+			attr.setHidden(false);
+			attr.setSystem(false);
+			return true;
+		}
+
+		bool FileDevice_WII_NAND::hasDir(const char* szDirPath) const
+		{
+			DVDDir dvdDirInfo;
+
+			s32 result = NANDChangeDir(szDirPath);
+
+			return result == NAND_RESULT_OK;
+		}
+
+		bool FileDevice_WII_NAND::createDir(const char* szDirPath) const
+		{
+			return false;
+		}
+
+
+
+		/*
+		static bool sIsDots(const char* str)
+		{
+			return (x_strcmp(str,".")==0 || x_strcmp(str,"..")==0);
+		}
+
+		struct enumerate_delegate_files_delete_dir : public enumerate_delegate<xfileinfo>
+		{
+			virtual void operator () (s32 depth, const xfileinfo& inf, bool& terminate)
+			{
+				terminate = false;
+				char systemFileBuffer[xdirpath::XDIRPATH_MAX];
+				xcstring systemFile(systemFileBuffer, sizeof(systemFileBuffer));
+				formatPath(inf.getFullName().c_str_device(),systemFile);
+				cellFsUnlink(systemFile.c_str());
+			}
+
+			using enumerate_delegate::operator();
+		};
+
+		struct enumerate_delegate_dirs_delete_dir : public enumerate_delegate<xdirinfo>
+		{
+			virtual void operator () (s32 depth, const xdirinfo& inf, bool& terminate)
+			{
+				terminate = false;
+				char systemDirBuffer[xfilepath::XFILEPATH_MAX];
+				xcstring systemDir(systemDirBuffer,sizeof(systemDirBuffer));
+				formatPath(inf.getFullName().c_str_device(),systemDir);
+				cellFsRmdir(systemDir.c_str()); // remove the empty directory
+			}
+
+			using enumerate_delegate::operator();
+		};
+
+		struct enumerate_delegate_dirs_copy_dir : public enumerate_delegate<xdirinfo>
+		{
+			cstack<const xdirinfo* > dirStack;
+			enumerate_delegate_dirs_copy_dir() { dirStack.init(getAllocator(),MAX_ENUM_SEARCH_DIRS); }
+			virtual ~enumerate_delegate_dirs_copy_dir() { dirStack.clear(); }
+ 			virtual void operator () (s32 depth, const xdirinfo& inf, bool& terminate)		{ }
+			virtual void operator() (s32 depth, const xdirinfo* inf, bool& terminate )
+			{
+				terminate = false;
+				dirStack.push(inf);
+			}
+		};
+
+		struct enumerate_delegate_files_copy_dir : public enumerate_delegate<xfileinfo>
+		{
+			cstack<const xfileinfo* > fileStack;
+			enumerate_delegate_files_copy_dir() { fileStack.init(getAllocator(),MAX_ENUM_SEARCH_FILES); }
+			virtual ~enumerate_delegate_files_copy_dir() { fileStack.clear(); }
+			virtual void operator () (s32 depth, const xfileinfo& inf, bool& terminate)	{ }
+			virtual void operator() (s32 depth, const xfileinfo* inf, bool& terminate ) 
+			{
+				terminate = false;
+				fileStack.push(inf);
+			}
+		};
+
+		static void changeDirPath(const char* szDirPath,const char* szToDirPath,const xdirinfo* szDirinfo,xdirpath& outDirPath)
+		{
+			xdirpath nDirpath_from(szDirPath);
+			xdirpath nDirpath_to(szToDirPath);
+			s32 depth1 = nDirpath_from.getLevels();
+			xdirpath parent,child;
+			szDirinfo->getFullName().split(depth1,parent,child);
+			nDirpath_to.getSubDir(child.c_str(),outDirPath);
+		}
+
+		static void changeFilePath(const char* szDirPath,const char* szToDirPath,const xfileinfo* szFileinfo,xfilepath& outFilePath)
+		{
+			xdirpath nDir;
+			szFileinfo->getFullName().getDirPath(nDir);
+			xdirpath nDirpath_from(szDirPath);
+			xdirpath nDirpath_to(szToDirPath);
+
+			xfilepath fileName = szFileinfo->getFullName();
+			fileName.onlyFilename();
+			s32 depth = nDirpath_from.getLevels();
+			xdirpath parent,child,copyDirPath_To;
+			nDir.split(depth,parent,child);
+			nDirpath_to.getSubDir(child.c_str(),copyDirPath_To);
+			outFilePath = xfilepath(copyDirPath_To,fileName);
+		}
+
+		static bool enumerateCopyDir(const char* szDirPath, bool boSearchSubDirectories, enumerate_delegate<xfileinfo>* file_enumerator, enumerate_delegate<xdirinfo>* dir_enumerator, s32 depth)
+		{
+			char DirPathBuffer[CELL_FS_MAX_FS_PATH_LENGTH+2];
+			char FileNameBuffer[CELL_FS_MAX_FS_PATH_LENGTH+2];
+			xcstring DirPath(DirPathBuffer,sizeof(DirPathBuffer),szDirPath);
+			DirPath += "/";
+
+			xcstring FileName(FileNameBuffer,sizeof(FileNameBuffer),szDirPath);
+			FileName +="/";
+			s32 nDirHandle;
+			cellFsOpendir(DirPath.c_str(),&nDirHandle);
+
+			DirPath = FileName;
+			bool bSearch = true;
+			while(bSearch)
+			{
+				uint64_t readSize;
+				CellFsDirent item;
+				s32 nResult = cellFsReaddir(nDirHandle,&item,&readSize);
+				if(nResult == CELL_FS_SUCCEEDED)
+				{
+					if (readSize == 0)
+						break;
+
+					if (sIsDots(item.d_name))
+						continue;
+
+					FileName += item.d_name;
+					if (item.d_type == CELL_FS_TYPE_DIRECTORY)
+					{
+						if (!enumerateCopyDir(FileName.c_str(),boSearchSubDirectories,file_enumerator,dir_enumerator,depth+1))
+						{
+							cellFsClosedir(nDirHandle);
+							return false;
+						}
+
+						FileName = DirPath;
+					}
+					else if (item.d_type == CELL_FS_TYPE_REGULAR)
+					{
+						xfileinfo* fi = new xfileinfo(FileName.c_str());
+						if (file_enumerator!=NULL && fi!=NULL)
+						{
+							bool terminate;
+							(*file_enumerator)(depth,fi,terminate);
+							bSearch = !terminate;
+						}
+						else
+						{
+							delete fi;		fi = NULL;
+						}
+						FileName = DirPath;
+					}
+				}
+				else
+				{
+					if (nResult == CELL_FS_EFAULT)
+					{
+							bSearch = false;
+					}
+					else
+					{
+						cellFsClosedir(nDirHandle);
+						return false;
+					}
+				}
+			}
+			cellFsClosedir(nDirHandle);
+
+			if (dir_enumerator!=NULL)
+			{
+				xdirinfo* di = new xdirinfo(FileName.c_str());
+				bool terminate;
+				if (di != NULL)
+				{
+					(*dir_enumerator)(depth,di,terminate);
+				}
+				else
+				{
+					delete di;			di = NULL;
+				}
+				bSearch = !terminate;
+			}
+
+			return true;
+		}
+		*/
+
+		bool FileDevice_WII_NAND::copyDir(const char* szDirPath, const char* szToDirPath, bool boOverwrite) const
+		{
+			
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::deleteDir(const char* szDirPath) const
+		{
+			s32 result = NANDDelete(szDirPath);
+
+			return result == NAND_RESULT_OK;			
+		}
+
+		bool FileDevice_WII_NAND::moveDir(const char* szDirPath, const char* szToDirPath) const
+		{
+			s32 result = NANDMove(szDirPath, szToDirPath);
+			return result == NAND_RESULT_OK;
+		}
+
+		bool FileDevice_WII_NAND::setDirTime(const char* szDirPath, const xdatetime& creationTime, const xdatetime& lastAccessTime, const xdatetime& lastWriteTime) const
+		{
+			// invoke cellFsUtime
+			// This API is not supported
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::getDirTime(const char* szDirPath, xdatetime& outCreationTime, xdatetime& outLastAccessTime, xdatetime& outLastWriteTime) const
+		{
+
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::setDirAttr(const char* szDirPath, const xattributes& attr) const
+		{
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::getDirAttr(const char* szDirPath, xattributes& attr) const
+		{
+			return false;
+		}
+
+		bool FileDevice_WII_NAND::enumerate(const char* szDirPath, bool boSearchSubDirectories, enumerate_delegate<xfileinfo>* file_enumerator, enumerate_delegate<xdirinfo>* dir_enumerator, s32 depth/* =0 */) const
+		{
+			return false;
+		}
+
+
 
 		//==============================================================================
 		// END xfilesystem namespace
