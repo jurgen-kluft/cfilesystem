29e8e4aaeb3e8b83920db40ea56964e6cca3bcaf
jurgen.kluft <jurgen.kluft@gmail.com>
2023-05-22 21:25:48 +0800

    * path; refactor to use red-black tree

--- source/main/include/cfilesystem/private/c_path.h
+++ source/main/include/cfilesystem/private/c_path.h
@@ -2,7 +2,7 @@
 #define __C_FILESYSTEM_XPATH_H__
 #include "ccore/c_target.h"
 #ifdef USE_PRAGMA_ONCE
-#pragma once
+#    pragma once
 #endif
 
 #include "ccore/c_debug.h"
@@ -67,103 +67,71 @@ namespace ncore
     struct filesysroot_t;
     struct pathname_t;
 
-    struct path_t
-    {
-        s32         m_ref;
-        s16         m_len;
-        s16         m_cap;
-        pathname_t* m_path[1];
-
-        inline path_t() : m_ref(0), m_len(0), m_cap(1) {}
-
-        pathname_t* get_name() const;
-        path_t*     prepend(pathname_t* folder, alloc_t* allocator);
-        path_t*     append(pathname_t* folder, alloc_t* allocator);
-        s32         compare(path_t* other) const;
-        void        to_string(runes_t& str) const;
-
-        path_t*        attach();
-        bool           detach(filesys_t* root);
-        static path_t* construct(alloc_t* allocator, s32 len);
-        static void    destruct(alloc_t* allocator, path_t* path);
-        static void    copy_array(pathname_t** from, u32 from_start, u32 from_len, pathname_t** dst, u32 dst_start);
-    };
-
+    // The whole path table should become a red-black tree and not a hash table.
+    // Every 'folder' has siblings (files and folders), each 'folder' sibling again
+    // has siblings (files and folders) and so on. The root folder is the only one
+    // that has no siblings. The root folder is the only one that has a null parent.
+    //
     struct pathname_t
     {
-        u64         m_hash;
-        pathname_t* m_next;
-        s32         m_refs;
-        s32         m_len;
-        utf32::rune m_name[2];
-
-        pathname_t();
-        pathname_t(s32 strlen);
-
-        bool isEmpty() const;
-
-        static s32 compare(const pathname_t* name, const pathname_t* other);
-        s32        compare(const pathname_t* other) const;
-        s32        compare(crunes_t const& name) const;
-
-        pathname_t*        attach();
-        bool               detach();
-        void               release(alloc_t* allocator);
-
-        void               to_string(runes_t& str) const;
-
-    protected:
-        friend class filesys_t;
-        static pathname_t* construct(alloc_t* allocator, u64 hname, crunes_t const& name);
-
-        DCORE_CLASS_PLACEMENT_NEW_DELETE
+        pathname_t* m_siblings[2]; // rbtree, left/right
+        u32         m_str_offset;  // offset into 'm_textdata'
+        u16         m_str_len;     // length of the string
+        u8          m_color;       // rbtree, color (0=red, 1=black)
+        u8          m_flags;       // flags
     };
 
     struct pathname_table_t
     {
         inline pathname_table_t() : m_len(0), m_cap(0), m_table(nullptr) {}
 
-        void        init(alloc_t* allocator, s32 cap = 65536);
-        void        release(alloc_t* allocator);
+        void init(alloc_t* allocator, s32 cap = 65536);
+        void release(alloc_t* allocator);
 
-        pathname_t* find(u64 hash) const;
-        pathname_t* next(u64 hash, pathname_t* prev) const;
-        void        insert(pathname_t* name);
-        bool        remove(pathname_t* name);
-        u32         hash_to_index(u64 hash) const;
+        pathname_t* find(utf32::pcrune item) const;
+        pathname_t* insert(utf32::pcrune item);
+        bool        remove(pathname_t*);
 
-        s32          m_len;
-        s32          m_cap;
-        pathname_t** m_table;
+        utf32::prune m_textdata;
+        pathname_t*  m_root;
+    };
+
+    struct pathnode_t
+    {
+        pathnode_t* m_parent;      // parent folder (not part of rbtree)
+        pathnode_t* m_siblings[2]; // rbtree, left/right
+        pathnode_t* m_children;    // rbtree, content root
+        u32         m_str_offset;  // file/folder string
+        u16         m_str_len;     // string length
+        u8          m_color;       // sibling color
+    };
+
+    struct paths_t
+    {
+        pathnode_t*      m_freenodes; // Should be a virtual memory array
+        pathname_table_t m_names;
+        pathname_table_t m_extensions;
     };
 
     struct pathdevice_t
     {
-        inline pathdevice_t()
-            : m_root(nullptr)
-            , m_alias(nullptr)
-            , m_deviceName(nullptr)
-            , m_devicePath(nullptr)
-            , m_redirector(nullptr)
-            , m_fileDevice(nullptr)
-        {
-        }
+        inline pathdevice_t() : m_root(nullptr), m_alias(nullptr), m_deviceName(nullptr), m_devicePath(nullptr), m_redirector(nullptr), m_fileDevice(nullptr) {}
 
-        void           init(filesys_t* owner);
-        pathdevice_t*  construct(alloc_t* allocator, filesys_t* owner);
-        pathdevice_t*  attach();
-        bool           detach(filesys_t* root);
-        void           destruct(alloc_t* allocator, pathdevice_t*& device);
-        s32            compare(pathdevice_t* device) const;
-        void           to_string(runes_t& str) const;
-        s32            to_strlen() const;
+        void          init(filesys_t* owner);
+        pathdevice_t* construct(alloc_t* allocator, filesys_t* owner);
+        pathdevice_t* attach();
+        bool          detach(filesys_t* root);
+        void          destruct(alloc_t* allocator, pathdevice_t*& device);
+        s32           compare(pathdevice_t* device) const;
+        void          to_string(runes_t& str) const;
+        s32           to_strlen() const;
 
-        filesys_t*    m_root;         // 
-        pathname_t*   m_alias;        // an alias redirection (e.g. "data")
-        pathname_t*   m_deviceName;   // "[appdir:\]data\bin.pc\", "[data:\]files\" to "[appdir:\]data\bin.pc\files\"
-        path_t*       m_devicePath;   // "appdir:\[data\bin.pc\]", "data:\[files\]" to "appdir:\[data\bin.pc\files\]"
-        pathdevice_t* m_redirector;   // If device path can point to another pathdevice_t
-        filedevice_t* m_fileDevice;   // or the final device (e.g. "e:\")
+        filesys_t*    m_root;       //
+        pathname_t*   m_alias;      // an alias redirection (e.g. "data")
+        pathname_t*   m_deviceName; // "[appdir:\]data\bin.pc\", "[data:\]files\" to "[appdir:\]data\bin.pc\files\"
+        pathnode_t*   m_devicePath; // "appdir:\[data\bin.pc\]", "data:\[files\]" to "appdir:\[data\bin.pc\files\]"
+        pathdevice_t* m_redirector; // If device path can point to another pathdevice_t
+        filedevice_t* m_fileDevice; // or the final device (e.g. "e:\")
     };
 }; // namespace ncore
 
